7aed92bb054f990bc3d0495250fc997e973857a5
super choque <thalesrjr5@gmail.com>
2023-05-30 17:41:58 -0300

    erro-novo

---
+++ app/helpers/companies_helper.rb
@@ -0,0 +1,10 @@
+module CompaniesHelper
+    # def dynamec_url_company
+    #     if current_user.company.present? && current_user.company.try(:id).present?
+    #         edit_compant_path(current_user.company)
+    #     else
+    #         new_company_path
+        
+    # end    
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+  get 'companies/edit'
+  get 'companies/create'
+  get 'companies/update'
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
