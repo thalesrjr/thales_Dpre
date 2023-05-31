cc53eab0decf55eaeb44a7874593b378324c3c60
super choque <thalesrjr5@gmail.com>
2023-05-31 16:53:38 -0300

    erro de fato

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'companies/new'
-  get 'companies/edit'
-  get 'companies/create'
-  get 'companies/update'
+  resources :applicants, only: [:index, :new, :create]
+  resources :positions
+  resources :companies, only: [:new, :edit, :create, :update]
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
